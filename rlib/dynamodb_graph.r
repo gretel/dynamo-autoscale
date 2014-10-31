3d87358f560878a75de524995ccec3e5f3392be2
tom hensel <tom@jitter.eu>
2014-10-31 01:18:23 +0100

    refactored methods related to graphing simplified logic use system() to call R tweaked R scripts bit tweaked signal hanlder log on EXIT cleanup

--- rlib/dynamodb_graph.r
+++ rlib/dynamodb_graph.r
@@ -1,5 +1,6 @@
 require(ggplot2)
 require(reshape)
+
 args <- commandArgs(trailingOnly = TRUE)
 data = read.csv(args[1], header=T, sep=",")
 
@@ -8,10 +9,7 @@ data$time = strptime(data$time, "%Y-%m-%dT%H:%M:%SZ")
 measure.vars = c('provisioned_reads','provisioned_writes',
                  'consumed_reads','consumed_writes')
 
-# measure.vars = c('consumed_reads','consumed_writes')
-
 ive.melted = melt(data, id.vars='time', measure.vars = measure.vars)
-
 g = ggplot(ive.melted, aes(x=time, y=value, color=variable)) + geom_line()
 
-ggsave(file=args[2], plot=g, width=20, height=8)
+ggsave(file=args[2], plot=g, width=12, height=8)
--- rlib/dynamodb_scatterplot.r
+++ rlib/dynamodb_scatterplot.r
@@ -1,13 +1,16 @@
 require(ggplot2)
 require(reshape)
+
 args <- commandArgs(trailingOnly = TRUE)
 data = read.csv(args[1], header=T, sep=",")
 
 data$time = strptime(data$time, "%Y-%m-%dT%H:%M:%SZ")
 data$hour = as.factor(strftime(data$time, "%H"))
-measure.vars = c(args[3])
+
+measure.vars = c('provisioned_reads','provisioned_writes',
+                 'consumed_reads','consumed_writes')
 
 ive.melted = melt(data, id.vars='hour', measure.vars = measure.vars)
 g = ggplot(ive.melted, aes(x=hour, y=value, color=variable)) + geom_point()
 
-ggsave(file=args[2], plot=g, width=10, height=8)
+ggsave(file=args[2], plot=g, width=12, height=8)
