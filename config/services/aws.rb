7fbcf7efa90bf8605ffdcbb859b8d06e9cffba6f
Tom Hensel <tom@jitter.eu>
2014-11-11 18:50:39 +0100

    use uppercase for "constants"

--- config/services/aws.rb
+++ config/services/aws.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk-v1'
 
-valid_regions = [
+VALID_REGIONS = [
   'ap-northeast-1', 'ap-southeast-1', 'ap-southeast-2',
   'us-east-1', 'us-west-1', 'us-west-2',
   'eu-central-1', 'eu-west-1',
@@ -14,7 +14,7 @@ raise DynamoAutoscale::Error::InvalidConfigurationError.new('You must specify a 
 
 DynamoAutoscale::Logger.logger.warn "Specified region '#{aws_config[:region]}'" +
   ' does not appear in the list of known regions.' +
-  ' Proceed with caution!' unless valid_regions.include?(aws_config[:region])
+  ' Proceed with caution!' unless VALID_REGIONS.include?(aws_config[:region])
 
 begin
   AWS.config(aws_config) if defined? AWS
