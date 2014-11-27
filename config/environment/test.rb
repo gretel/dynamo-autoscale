d5db3838b0547545dbf3e39cd34d138904dee237
Tom Hensel <tom@jitter.eu>
2014-11-27 14:22:14 +0100

    use credentials from ENV

--- config/environment/test.rb
+++ config/environment/test.rb
@@ -1,12 +1,18 @@
-require 'timecop'
 require 'simplecov'
 SimpleCov.start
+
+# require 'timecop'
+
+raise RuntimeError.new('you need to supply AWS_ACCESS_KEY, aborting') if ENV['AWS_ACCESS_KEY'].nil?
+raise RuntimeError.new('you need to supply AWS_SECRET_KEY, aborting') if ENV['AWS_SECRET_KEY'].nil?
 
 $logger_level = :error
 TEST_CONFIG = 'dynamo-autoscale-test.yml'
 
 require_relative 'common'
-DynamoAutoscale.setup_from_config(DynamoAutoscale.config_dir(TEST_CONFIG), { :dry_run => true })
+DynamoAutoscale.setup_from_config(DynamoAutoscale.config_dir(TEST_CONFIG), { :dry_run => true,
+                                                                             :aws => { :access_key_id => ENV['AWS_ACCESS_KEY'],
+                                                                                       :secret_access_key => ENV['AWS_SECRET_KEY'] } } )
 DynamoAutoscale.require_in_order(
   'spec/helpers/**.rb'
 )
