36d8790e8821cad8ae3837dbf8361aa0d7fcc918
Tom Hensel <tom@jitter.eu>
2014-12-11 15:23:57 +0100

    use mono_logger

--- lib/dynamo-autoscale/logger.rb
+++ lib/dynamo-autoscale/logger.rb
@@ -1,13 +1,13 @@
-require 'yell'
+# dont buffer these, might be important
+STDERR.sync = true
+
+require_relative 'ext/yell/mono_logger_adapter'
 
 # TODO: abstraction
 LOGGER_ID='dynamo'
 LOGGER_LEVEL_DEFAULT = :info
 LOGGER_FORMAT = "%d [%5L] %p %h : %m"
 LOGGER_TIME_FORMAT = "%F %T.%L"
-
-# dont buffer these, might be important
-STDERR.sync = true
 
 # enforce logging level defaults if unset
 $logger_level ||= LOGGER_LEVEL_DEFAULT
@@ -18,7 +18,8 @@ module DynamoAutoscale
       # see https://github.com/rudionrails/yell .. https://github.com/rudionrails/yell/wiki/101-formatting-log-messages
       @@logger ||= Yell.new do |l|
         l.name = LOGGER_ID
-        l.adapter :stdout, level: "gte.#{$logger_level}", format: Yell.format(LOGGER_FORMAT, LOGGER_TIME_FORMAT)
+        l.adapter :mono_logger_adapter, level: "gte.#{$logger_level}", format: Yell.format(LOGGER_FORMAT, LOGGER_TIME_FORMAT)
+        # l.adapter :stdout, level: "gte.#{$logger_level}", format: Yell.format(LOGGER_FORMAT, LOGGER_TIME_FORMAT)
         # l.adapter :stderr, level: [:error, :fatal], format: Yell.format(LOGGER_FORMAT, LOGGER_TIME_FORMAT)
       end
       @@logger
@@ -27,10 +28,6 @@ module DynamoAutoscale
     def logger
       DynamoAutoscale::Logger.logger
     end
-
-    # def logger= new_logger
-    #   DynamoAutoscale::Logger.logger = new_logger
-    # end
 
     def self.included base
       base.extend DynamoAutoscale::Logger
